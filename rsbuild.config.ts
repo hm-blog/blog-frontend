ca05b89e8c763f576ab04de85b88e02015b6f15b
정회민 <wony1102@naver.com>
2025-01-25 16:45:39 +0900

    style: change port

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,15 +1,18 @@
 import { defineConfig, loadEnv } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
-const { publicVars } = loadEnv({prefixes: ['REACT_APP_']})
+const { publicVars } = loadEnv({ prefixes: ['REACT_APP_'] });
 
 export default defineConfig({
   plugins: [pluginReact()],
   dev: {
     hmr: false,
-    liveReload: true
+    liveReload: true,
   },
   source: {
     define: publicVars,
-  }
+  },
+  server: {
+    port: 9300,
+  },
 });
